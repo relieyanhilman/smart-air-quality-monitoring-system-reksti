203b6439195c8b6ef889a2206459ac2163657753
relieyanhilman <relieyanhilman93@gmail.com>
2022-04-22 21:31:02 +0800

    coba pake chartjs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
+import React, {useState} from 'react';
+import "./App.css";
+import BarChart from "./components/BarChart";
+import LineChart from "./components/LineChart";
+
+import {UserData} from './Data'
+
+
 function App() {
+
+  const [userData, setUserData] = useState({
+    labels: UserData.map((data) => data.year),
+    datasets: [
+      {
+      label: "Users Gained",
+      data: UserData.map((data) => data.userGain),
+      backgroundColor: ["rgba(75,192,192,1)",
+        "#ecf0f1",
+        "#50AF95",
+        "#f3ba2f",
+        "#2a71d0"
+    ],
+    borderColor: "black",
+    borderWidth: 2
+    },
+  ],
+  });
   return (
     <div className="App">
-      <p>hello</p>
+      <div style={{width: 700}}>
+      <BarChart chartData={userData} />
+      </div>
+      <div style={{width: 700}}>
+      <LineChart chartData={userData} />
+      </div>
     </div>
   );
 }
---
+++ src/Data.js
@@ -0,0 +1,31 @@
+export const UserData = [{
+        id: 1,
+        year: 2016,
+        userGain: 60000,
+        userLost: 898,
+    },
+    {
+        id: 2,
+        year: 2017,
+        userGain: 76600,
+        userLost: 216,
+    },
+    {
+        id: 3,
+        year: 2018,
+        userGain: 93600,
+        userLost: 300,
+    },
+    {
+        id: 4,
+        year: 2019,
+        userGain: 75000,
+        userLost: 148,
+    },
+    {
+        id: 5,
+        year: 2020,
+        userGain: 86500,
+        userLost: 248,
+    }
+]
